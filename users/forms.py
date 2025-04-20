5ad7a3b51de5fca08749b245e2cffe0a951c89e5
nurmuhammad <nurmuhammadsamadov63@gmail.com>
2025-04-20 10:28:04 +0500

    connect f a b register

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,46 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
+
+# class UserRegisterForm(UserCreationForm):
+#     phone_number = forms.CharField(
+#         max_length=15,
+#         required=False,
+#         widget=forms.TextInput(attrs={
+#             'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+#             'placeholder': 'Phone number:'
+#         })
+#     )
+
+#     role = forms.ChoiceField(
+#         choices=User.ROLE_CHOICES,
+#         widget=forms.Select(attrs={
+#             'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+#         })
+#     )
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2', 'phone_number', 'role']
+#         widgets = {
+#             'username': forms.TextInput(attrs={
+#                 'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+#                 'placeholder': 'Username:',
+#             }),
+#             'email': forms.EmailInput(attrs={
+#                 'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+#                 'placeholder': 'Email:',
+#             }),
+#             'password1': forms.PasswordInput(attrs={
+#                 'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+#                 'placeholder': 'Password:',
+#             }),
+#             'password2': forms.PasswordInput(attrs={
+#                 'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+#                 'placeholder': 'Confirm Password:',
+#             }),
+#         }
+
 
 class UserRegisterForm(UserCreationForm):
     phone_number = forms.CharField(
@@ -12,16 +52,27 @@ class UserRegisterForm(UserCreationForm):
         })
     )
 
-    role = forms.ChoiceField(
-        choices=User.ROLE_CHOICES,
-        widget=forms.Select(attrs={
+    # role = forms.ChoiceField(
+    #     choices=User.ROLE_CHOICES,
+    #     widget=forms.Select(attrs={
+    #         'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+    #     })
+    # )
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({
             'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+            'placeholder': 'Password:'
         })
-    )
+        self.fields['password2'].widget.attrs.update({
+            'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+            'placeholder': 'Confirm Password:'
+        })
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'phone_number', 'role']
+        fields = ['username', 'phone_number' , 'password1', 'password2']
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
@@ -31,12 +82,17 @@ class UserRegisterForm(UserCreationForm):
                 'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
                 'placeholder': 'Email:',
             }),
-            'password1': forms.PasswordInput(attrs={
-                'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
-                'placeholder': 'Password:',
-            }),
-            'password2': forms.PasswordInput(attrs={
-                'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
-                'placeholder': 'Confirm Password:',
-            }),
         }
+
+
+class StyledAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(StyledAuthenticationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+            'placeholder': 'Username:'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'bg-[#008CFF59] py-4 px-8 rounded-full w-full form-control',
+            'placeholder': 'Password:'
+        })
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, get_user_model
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, StyledAuthenticationForm
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm
 from django.contrib.auth.decorators import login_required
 from django.views import View
@@ -25,17 +25,27 @@ def user_register(request):
 
 
 
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = AuthenticationForm()
+#     return render(request, 'register/index.html', {'form': form})
+
 def user_login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = StyledAuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('home')
     else:
-        form = AuthenticationForm()
+        form = StyledAuthenticationForm()
     return render(request, 'register/index.html', {'form': form})
-
 
 
 
